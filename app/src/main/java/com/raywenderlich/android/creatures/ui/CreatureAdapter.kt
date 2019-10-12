5be8b297d4e3924dabcc3ff4d135a18c5cc6accf
Henry AT <yiperu@gmail.com>
2019-10-12 09:18:18 -0300

    👨🏻‍💻  Implement three member for default

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -2,9 +2,22 @@ package com.raywenderlich.android.creatures.ui
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import android.view.ViewGroup
 import com.raywenderlich.android.creatures.model.Creature
 
 class CreatureAdapter(private val creatures: List<Creature>) : RecyclerView.Adapter<CreatureAdapter.ViewHolder>() {
+
+  override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun getItemCount(): Int {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+
+  override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
 
   class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
 
