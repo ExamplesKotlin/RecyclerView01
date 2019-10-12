7e4f3990def9a690d32c601ed9b2085317dd5f69
Henry AT <yiperu@gmail.com>
2019-10-12 09:14:09 -0300

    🗜  Adapter -Create CreatureAdapter class and inside a ViewHolder class

---
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.android.creatures.ui
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class CreatureAdapter() {
+
+  class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+  }
+}
