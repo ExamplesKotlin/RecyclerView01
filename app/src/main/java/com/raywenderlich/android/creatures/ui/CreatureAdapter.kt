5883941e5373d35d1f9f2a809406ca89f0b2de52
Henry AT <yiperu@gmail.com>
2019-10-12 09:22:26 -0300

    👨🏻‍💻  Implement some methods

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -3,17 +3,17 @@ package com.raywenderlich.android.creatures.ui
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.raywenderlich.android.creatures.R
+import com.raywenderlich.android.creatures.app.inflate
 import com.raywenderlich.android.creatures.model.Creature
 
 class CreatureAdapter(private val creatures: List<Creature>) : RecyclerView.Adapter<CreatureAdapter.ViewHolder>() {
 
-  override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
+    return ViewHolder(parent.inflate(R.layout.list_item_creature))
   }
 
-  override fun getItemCount(): Int {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-  }
+  override fun getItemCount() = creatures.size
 
   override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
     TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
