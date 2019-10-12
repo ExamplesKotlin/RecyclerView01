35ac110440f852c93a7aac3dd795300fe3ea4b99
Henry AT <yiperu@gmail.com>
2019-10-12 09:17:38 -0300

    👨🏻‍💻  Exten from RecyclerView.Adapter<CreatureAdapter.ViewHolder> and init constructor

--- app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -2,8 +2,9 @@ package com.raywenderlich.android.creatures.ui
 
 import android.support.v7.widget.RecyclerView
 import android.view.View
+import com.raywenderlich.android.creatures.model.Creature
 
-class CreatureAdapter() {
+class CreatureAdapter(private val creatures: List<Creature>) : RecyclerView.Adapter<CreatureAdapter.ViewHolder>() {
 
   class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
 
